c24b1e3b0bd2aa9818fcc44d35afac9cd7d5b2db
packtpavanr <pavanr@packtpub.com>
2016-11-30 15:17:28 +0530

    Code files updated

--- Module 3/Chapter_05/YASTU/mobile/src/main/java/com/plattysoft/yass/engine/particles/ParticleModifier.java
+++
@@ -1,8 +0,0 @@
-package com.plattysoft.yass.engine.particles;
-
-/**
- * Created by Raul Portales on 02/04/15.
- */
-public interface ParticleModifier {
-    void apply(Particle particle, long miliseconds);
-}
